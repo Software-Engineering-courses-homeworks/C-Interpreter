3b18c4a173bcf3d9aad1bb3f5e01badde9c37f1c
NathTro <tros1009@gmail.com>
2024-10-23 17:10:54 +0300

    added to compile function

--- compiler.c
+++ compiler.c
@@ -21,11 +21,13 @@ static void errorAt(Token* token, const char* message) {
     if (token->type == TOKEN_EOF) {
         fprintf(stderr, " at end");
     } else if (token->type == TOKEN_ERROR) {
-        //Nothing.
-    }else {
-        fprintf(stderr, ": %s\n", message);
-        parser.hadError = true;
+        // Nothing.
+    } else {
+        fprintf(stderr, " at '%.*s'", token->length, token->start);
     }
+
+    fprintf(stderr, ": %s\n", message);
+    parser.hadError = true;
 
     fprintf(stderr, ": %s\n", message);
     parser.hadError = true;
@@ -63,30 +65,11 @@ static void advance() {
 /// @return returns whether the code had error in it
 bool compile(const char *source, Chunk* chunk) {
     //initializes the scanner with the source string
+    ///TO BE RECORDED
     initScanner(source);
+    advance();
+    expression();
+    consume(TOKEN_EOF,"Expect end of expression");
 
-    //sets the line to -1
-    int line = -1;
-
-    //runs the scanner and generates tokens on the fly
-    for(;;) {
-        //scans the lexeme and saves the return token
-        Token token = scanToken();
-
-        //if the line is differnt then the last line printed, print is and update 'line'
-        //else, print a | to indicate it's the same line of code
-        if(token.line != line) {
-            printf("%4d ", token.line);
-            line = token.line;
-        }
-        else {
-            printf("    | ");
-        }
-        //prints the operation token
-        printf("%2d '%.*s'\n", token.type, token.length, token.start);
-
-        //stop the loop when reaching EOF
-        if(token.type == TOKEN_EOF)
-            break;
-    }
+   return !parser.hadError;
 }
