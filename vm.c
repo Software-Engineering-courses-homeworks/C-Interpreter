e9aadbafb3cd5eb0a698e4210f02d8d4bf673ce3
ishay <141156460+idshay16@users.noreply.github.com>
2024-10-30 15:59:16 +0200

    added debug printouts for the comparison opcodes in debug.c  added support for comparison logic in compiler.c  added support for comparison logic in vm.c

--- vm.c
+++ vm.c
@@ -159,14 +159,14 @@ do { \
                 push(BOOL_VAL(valuesEqual(a,b)));
                 break;
             }
+            case OP_GREATER: BINARY_OP(BOOL_VAL, >); break;
+            case OP_LESS: BINARY_OP(BOOL_VAL, <); break;
             //cases for arithmetic operations
             case OP_ADD: BINARY_OP(NUMBER_VAL, +); break;
             case OP_SUBTRACT: BINARY_OP(NUMBER_VAL, -); break;
             case OP_MULTIPLY: BINARY_OP(NUMBER_VAL,*); break;
             case OP_DIVIDE: BINARY_OP(NUMBER_VAL, /); break;
-            case OP_NOT:
-                push(BOOL_VAL(isFalsey(pop())));
-            break;
+            case OP_NOT: push(BOOL_VAL(isFalsey(pop()))); break;
         }
     }
 #undef READ_BYTE
