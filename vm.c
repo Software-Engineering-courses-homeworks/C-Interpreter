6aa0a039930dd57d538874c9049e97d1aaf094a0
NathTro <tros1009@gmail.com>
2024-10-30 15:28:25 +0200

    added case nil/true/false to switch case in run function and also Tokens for them in the table in compiler.c

--- vm.c
+++ vm.c
@@ -141,6 +141,10 @@ do { \
                 //printf("'\n");
                 break;
             }
+            //cases for nil,false and true
+            case OP_NIL: push(NIL_VAL); break;
+            case OP_TRUE:push(BOOL_VAL(true));break;
+            case OP_FALSE:push(BOOL_VAL(false));break;
             //cases for arithmetic operations
             case OP_ADD: BINARY_OP(NUMBER_VAL, +); break;
             case OP_SUBTRACT: BINARY_OP(NUMBER_VAL, -); break;
