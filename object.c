7fb2fd1dc6bb29218f0c91743baa38348da0c955
ishay <141156460+idshay16@users.noreply.github.com>
2024-11-04 18:37:12 +0200

    implemented the hash table modules, structs, and base functionality

--- object.c
+++ object.c
@@ -9,7 +9,7 @@
 #define ALLOCATE_OBJ(type, objectType) \
     (type*)allocateObject(sizeof(type), objectType)
 
-/// The function allocated memory for a new object and initalizes its type field
+/// The function allocated memory for a new object and initializes its type field
 /// @param size
 /// @param type
 /// @return a new object
@@ -25,19 +25,38 @@ static Obj* allocateObject(size_t size, ObjType type) {
 /// @param chars
 /// @param length
 /// @return a new string
-static Obj* allocateString(char* chars, int length) {
+static Obj* allocateString(char* chars, int length, uint32_t hash) {
     ObjString* string = ALLOCATE_OBJ(ObjString, OBJ_STRING);
     string -> length = length;
     string -> chars = chars;
+    string -> hash = hash;
     return string;
 }
 
-/// The function recieves an array of characters and a length, and returns a new string from the array
+/// hashes a given string using the FNV-1a hash function
+/// @param key the string key
+/// @param length the string key length
+/// @return a uint32_t hash key
+static uint32_t hashString(char* key, int length) {
+    //the hash function's bse prime
+    uint32_t hash = 2166136261u;
+
+    //hashes the string by going over each character in the key
+    for (int i = 0; i < length; i++) {
+        hash ^= (uint8_t)key[i];
+        hash *= 16777619;
+    }
+
+    return hash;
+}
+
+/// The function receives an array of characters and a length, and returns a new string from the array
 /// @param chars
 /// @param length
 /// @return a new string
 ObjString* takeString(char* chars, int length) {
-    return allocateString(chars, length);
+    uint32_t hash = hashString(chars, length);
+    return allocateString(chars, length, hash);
 }
 
 /// The function copies a string from a given character array into a new allocated memory location
@@ -45,8 +64,10 @@ ObjString* takeString(char* chars, int length) {
 /// @param length
 /// @return a new string
 ObjString* copyString(const char* chars, int length) {
+    // hashes the new string before caching
+    uint32_t hash = hashString(chars, length);
 
-    //allocate memory on the heap for  the new stringa
+    //allocate memory on the heap for  the new string
     char* heapChars = ALLOCATE(char, length + 1);
 
     //copies the string contents to the newly allocated memory
@@ -55,7 +76,7 @@ ObjString* copyString(const char* chars, int length) {
     //adds a null terminator at the end
     heapChars[length] = '\0';
 
-    return allocateString(heapChars,length);
+    return allocateString(heapChars, length, hash);
 }
 
 /// prints the given Obj value according to it's type
---
+++ table.h
@@ -0,0 +1,51 @@
+#ifndef TABLE_H
+#define TABLE_H
+
+#include "common.h"
+#include "value.h"
+
+typedef struct {
+    ObjString* key;
+    Value value;
+}Entry;
+
+typedef struct {
+    int count;
+    int capacity;
+    Entry* entries;
+}Table;
+
+/// initializes the hash table
+/// @param table the VM's hash table
+void initTable(Table* table);
+
+/// frees the given hash table
+/// @param table the VM's hash table
+void freeTable(Table* table);
+
+/// the function gets a table, key and value and sets the value pointer value as the saved one in the key
+/// @param table the hash table
+/// @param key the key
+/// @param value the return value
+/// @return true if the key ahs been found, false otherwise
+bool tableGet(Table* table, ObjString* key, Value* value);
+
+/// the function gets a key/value pair and adds it to the hash table
+/// @param table the VM hash table
+/// @param key the key string
+/// @param value the hash value
+/// @return true if the new entry was added, false otherwise
+bool tableSet(Table* table, ObjString* key, Value value);
+
+/// the function gets a table and a key and deletes it form the table
+/// @param table the hash table
+/// @param key the key that needs to be removed
+/// @return true if the deletion was successful, false otherwise
+bool tableDelete(Table* table, ObjString* key);
+
+/// transfers values from one hash table to another
+/// @param from the origin table
+/// @param to the destination table
+void tableAddAll(Table* from, Table* to);
+
+#endif //TABLE_H
