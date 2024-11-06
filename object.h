a43be39dab355e44a09470d40609b4a7899b92b5
NathTro <tros1009@gmail.com>
2024-11-06 12:06:33 +0200

    fixed bugs

--- object.h
+++ object.h
@@ -7,7 +7,7 @@
 #define OBJ_TYPE(value) (AS_OBJ(value)->type)
 #define IS_STRING(value) isObjType(value, OBJ_STRING)
 #define AS_STRING(value) ((ObjString*)AS_OBJ(value))
-#define AS_CSTRING(value) (((ObjCString*)AS_OBJ(value))->chars)
+#define AS_CSTRING(value) (((ObjString*)AS_OBJ(value))->chars)
 
 typedef enum {
     OBJ_STRING,
